7f4f5625ad0ce4d4404df8e74e4f02e5acebf3dd
Chris Wilson <chrisw@myself.com>
2023-02-28 22:00:56 -0500

    lots to do tomorrow

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,40 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateMarkdown = require('./utils/generateMarkdown');
+const tryAgain = 'Please enter a value to continue';
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+const questions = [
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title",
+        validate: (value) => value ? true : tryAgain
+    },
+    {
+        type: "input",
+        message: "Do you have a table of contents",
+        name: "table of contents",
+    },
+    {
+        type: "input",
+        message: "How would you describe your project?",
+        name: "description",
+        validate: (value) => value ? true : tryAgain
+    },
+    { 
+        type: "input",
+        message: "How do you use this?",
+        name: "usage",
+        validate: (value) => value ? true : tryAgain
+    },
+    {
+        type: "list",
+        message: "What license did you use",
+        name: "license",
+        chocies: ['MIT License', 'GNU License', 'GPL License', 'Apache License'],
+        validate: (value) => value ? true : 'N/A'
+    },
+    {
+        
+    }
+];
