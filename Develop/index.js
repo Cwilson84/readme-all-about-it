2be6d4fb60a642724969b289a214ea60ba3c61b5
Chris Wilson <chrisw@myself.com>
2023-03-01 16:37:26 -0500

    index.js complete

--- Develop/index.js
+++ Develop/index.js
@@ -21,11 +21,17 @@ const questions = [
         name: "description",
         validate: (value) => value ? true : tryAgain
     },
-    { 
+    {
         type: "input",
         message: "How do you use this?",
         name: "usage",
         validate: (value) => value ? true : tryAgain
+    },
+    {
+        type: "input",
+        message: "What are the steps (if any) for installation?",
+        name: "installation",
+        validate: (value) => value ? true : 'N/A'
     },
     {
         type: "list",
@@ -35,6 +41,47 @@ const questions = [
         validate: (value) => value ? true : 'N/A'
     },
     {
-        
-    }
+        type: "input",
+        message: "How may others contribute, if at all?",
+        name: "contributions",
+        validate: (value) => value ? true : 'N/A'
+    },
+    {
+        type: "input",
+        message: "Are there any testing instructions?",
+        name: "tests",
+    },
+    {
+        type: "input",
+        message: "What Email address do you prefer to be contacted at reguarding this project?",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is your Github user name?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "Any credit or acknowledgement you'd like to give?",
+        name: "credits",
+    },
 ];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => 
+        err ? console.log(err) : console.log('Successfully generated your ReadMe!')
+        )
+};
+
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+        let content = generateMarkdown(data)
+        writeToFile('readMe.md', content)
+    }) .catch(function(err) {
+        console.log(err)
+    });
+};
+
+init();
